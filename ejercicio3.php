8314e1ee2216cc4aba04d75fdd8c667260806c3e
enadisfernandez <30443115+enadisfernandez@users.noreply.github.com>
2017-08-23 12:39:50 -0500

    Add files via upload

---
+++ ejercicio3.php
@@ -0,0 +1,27 @@
+<!--realice un  algoritmo que calcule el area y el perimetro de un deltoide y lo muestre en pantalla.-->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ejercicio 3</title>
+  </head>
+  <body>
+    <?php
+          $valor['l1']="6";
+          $valor['l2']="5";
+          $valor['l3']="2";
+          $valor['l4']="4";
+
+          $area=($valor['l1']*$valor['l2']/2);
+          echo "$valor[l1] , $valor[l2] </br> ";
+          echo "El Área del Deltoide es: $area </br>";
+
+          $perimetro=2*($valor['l3']+$valor['l4']);
+          echo "$valor[l3] , $valor[l4] </br> ";
+          echo "El perimetro del Deltoide es: $perimetro";
+
+     ?>
+
+  </body>
+</html>
---
+++ ejercicio6.php
@@ -0,0 +1,20 @@
+<!--desarrolla un algoritmo que pida un entero lo eleve al cubo y lo muestre enb pantalla.-->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ejercicio 6</title>
+  </head>
+  <body>
+  <?php
+         $valor[]="4";
+
+         $cubo=($valor[0]*$valor[0]*$valor[0]);
+         echo "$valor[0] </br>";
+          echo "El cubo del número es: $cubo </br>";
+
+
+   ?>
+  </body>
+</html>
